7c19b04c2d5ae3da8bc8e42c4d0f4c0284ace429
Alex-NTI <N0753537@MY.NTU.AC.UK>
2020-04-06 01:10:02 +0100

    New

---
+++ app/src/main/java/com/example/educat/Notes/NoteMainActivity.java
@@ -0,0 +1,87 @@
+package com.example.educat.Notes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.educat.R;
+
+import java.util.List;
+
+public class NoteMainActivity extends AppCompatActivity{
+
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    com.example.educat.Notes.Adapter adapter;
+    TextView noItemText;
+    NoteDatabase simpleDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_main);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+        noItemText = findViewById(R.id.noItemText);
+        simpleDatabase = new NoteDatabase(this);
+        List<Note> allNotes = simpleDatabase.getAllNotes();
+        recyclerView = findViewById(R.id.allNotesList);
+
+        if(allNotes.isEmpty()){
+            noItemText.setVisibility(View.VISIBLE);
+        }else {
+            noItemText.setVisibility(View.GONE);
+            displayList(allNotes);
+        }
+
+    }
+
+    private void displayList(List<Note> allNotes) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(this,allNotes);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.add){
+            Toast.makeText(this, "Add New Note", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this,AddNotes.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        List<Note> getAllNotes = simpleDatabase.getAllNotes();
+        if(getAllNotes.isEmpty()){
+            noItemText.setVisibility(View.VISIBLE);
+        }else {
+            noItemText.setVisibility(View.GONE);
+            displayList(getAllNotes);
+        }
+
+
+    }
+
+}
